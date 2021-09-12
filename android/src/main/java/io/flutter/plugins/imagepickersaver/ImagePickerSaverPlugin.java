5fe0650c1d86849b71d2d97978a91eb3a238639b
joezhangss <40815404+joezhangss@users.noreply.github.com>
2021-09-12 14:31:52 +0800

    适配flutter2.5.0
    适配flutter2.5.0

--- android/src/main/java/io/flutter/plugins/imagepickersaver/ImagePickerSaverPlugin.java
+++ android/src/main/java/io/flutter/plugins/imagepickersaver/ImagePickerSaverPlugin.java
@@ -4,52 +4,62 @@
 
 package io.flutter.plugins.imagepickersaver;
 
+import android.app.Activity;
 import android.os.Environment;
 
 import java.io.File;
 import java.io.IOException;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
+
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 
 
-public class ImagePickerSaverPlugin implements MethodChannel.MethodCallHandler {
+public class ImagePickerSaverPlugin implements MethodChannel.MethodCallHandler,FlutterPlugin, ActivityAware {
     private static final String CHANNEL = "plugins.flutter.io/image_picker_saver";
 
     private static final int SOURCE_CAMERA = 0;
     private static final int SOURCE_GALLERY = 1;
 
-    private final PluginRegistry.Registrar registrar;
-    private final ImagePickerDelegate delegate;
+//    private final PluginRegistry.Registrar registrar;
+//    private final ImagePickerDelegate delegate;
+    private ImagePickerDelegate delegate;
+    private  MethodChannel channel;
+    private Activity activity;
 
-    public static void registerWith(PluginRegistry.Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL);
+//    public static void registerWith(PluginRegistry.Registrar registrar) {
+//        final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL);
+//
+//        final File externalFilesDirectory =
+//                registrar.activity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        final ExifDataCopier exifDataCopier = new ExifDataCopier();
+//        final ImageResizer imageResizer = new ImageResizer(externalFilesDirectory, exifDataCopier);
+//
+//        final ImagePickerDelegate delegate =
+//                new ImagePickerDelegate(registrar.activity(), externalFilesDirectory, imageResizer);
+//        registrar.addActivityResultListener(delegate);
+//        registrar.addRequestPermissionsResultListener(delegate);
+//
+////        final ImagePickerSaverPlugin instance = new ImagePickerSaverPlugin(registrar, delegate);
+//        channel.setMethodCallHandler(instance);
+//    }
 
-        final File externalFilesDirectory =
-                registrar.activity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        final ExifDataCopier exifDataCopier = new ExifDataCopier();
-        final ImageResizer imageResizer = new ImageResizer(externalFilesDirectory, exifDataCopier);
-
-        final ImagePickerDelegate delegate =
-                new ImagePickerDelegate(registrar.activity(), externalFilesDirectory, imageResizer);
-        registrar.addActivityResultListener(delegate);
-        registrar.addRequestPermissionsResultListener(delegate);
-
-        final ImagePickerSaverPlugin instance = new ImagePickerSaverPlugin(registrar, delegate);
-        channel.setMethodCallHandler(instance);
-    }
-
-    @VisibleForTesting
-    ImagePickerSaverPlugin(PluginRegistry.Registrar registrar, ImagePickerDelegate delegate) {
-        this.registrar = registrar;
-        this.delegate = delegate;
-    }
+//    @VisibleForTesting
+//    ImagePickerSaverPlugin(PluginRegistry.Registrar registrar, ImagePickerDelegate delegate) {
+//        this.registrar = registrar;
+//        this.delegate = delegate;
+//    }
 
     @Override
     public void onMethodCall(MethodCall call, MethodChannel.Result result) {
-        if (registrar.activity() == null) {
+//        if (registrar.activity() == null) {
+        if (activity == null) {
             result.error("no_activity", "image_picker plugin requires a foreground activity.", null);
             return;
         }
@@ -91,5 +101,51 @@ public class ImagePickerSaverPlugin implements MethodChannel.MethodCallHandler {
         } else {
             throw new IllegalArgumentException("Unknown method " + call.method);
         }
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+
+        channel = new MethodChannel(binding.getBinaryMessenger(), CHANNEL);
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+        //        final File externalFilesDirectory = registrar.activity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        final File externalFilesDirectory = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        final ExifDataCopier exifDataCopier = new ExifDataCopier();
+        final ImageResizer imageResizer = new ImageResizer(externalFilesDirectory, exifDataCopier);
+
+//        final ImagePickerDelegate delegate = new ImagePickerDelegate(registrar.activity(), externalFilesDirectory, imageResizer);
+        final ImagePickerDelegate delegate = new ImagePickerDelegate(activity, externalFilesDirectory, imageResizer);
+//        registrar.addActivityResultListener(delegate);
+        binding.addActivityResultListener(delegate);
+//        registrar.addRequestPermissionsResultListener(delegate);
+        binding.addRequestPermissionsResultListener(delegate);
+
+//        final ImagePickerSaverPlugin instance = new ImagePickerSaverPlugin(registrar, delegate);
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        onAttachedToActivity(binding);
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
     }
 }
